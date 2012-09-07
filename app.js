dbb8a6a94318024417a771cc508d05889f577f67
U-KogMaw\Squirrel <Squirrel@KogMaw.(none)>
2012-09-06 21:35:48 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+(function(){
+	
+	var http = require('http');
+	var io = require('socket.io');
+	var fs = require('fs');	
+
+	var hub = require('./lib/socketHub.js');
+
+	var app = http.createServer(function handler (req, res) {
+		fs.readFile('./lib/static/index.html', function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+
+			res.writeHead(200);
+			res.end(data);
+		});
+	});
+	
+	var sockets = io.listen(app).sockets;
+
+	sockets.on('connection', function (socket) {
+		hub.receive(socket);
+	});
+
+	app.listen(80);
+
+})();
